c5095809d65fc9aaf2a6d4ca512da25fa3e0629d
Denys Halenok <denys.halenok@gmail.com>
2024-02-12 10:11:48 +0100

    Tweak colors integrations

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,5 +1,12 @@
 require("catppuccin").setup({
 	transparent_background = true,
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		treesitter = true,
+		which_key = true,
+		ufo = true,
+	},
 })
 
 vim.cmd.colorscheme("catppuccin")
