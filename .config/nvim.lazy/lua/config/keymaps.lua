582522cad6fd177e894fd3a36f7352cd42a913ef
Denys Halenok <denys.halenok@gmail.com>
2025-05-30 12:43:11 +0200

    Tweak movelines keymaps

--- .config/nvim.lazy/lua/config/keymaps.lua
+++ .config/nvim.lazy/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "K", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
