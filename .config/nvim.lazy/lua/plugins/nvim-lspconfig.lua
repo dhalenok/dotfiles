818559d1e6cb369aad76ead5d325f68c94c878d0
Denys Halenok <denys.halenok@gmail.com>
2025-06-06 22:04:25 +0200

    Remove lua LSP config

--- .config/nvim.lazy/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim.lazy/lua/plugins/nvim-lspconfig.lua
@@ -59,43 +59,12 @@ return {
       -- LSP Server Settings
       ---@type lspconfig.options
       servers = {
-        tailwindcss = {
+        eslint = {
           root_dir = function()
             return vim.fs.dirname(vim.fs.find(".git", { path = startpath, upward = true })[1])
           end,
         },
-        lua_ls = {
-          -- mason = false, -- set to false if you don't want this server to be installed with mason
-          -- Use this to add any additional keymaps
-          -- for specific lsp servers
-          -- ---@type LazyKeysSpec[]
-          -- keys = {},
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              codeLens = {
-                enable = true,
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-              doc = {
-                privateName = { "^_" },
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-            },
-          },
-        },
-        eslint = {
+        tailwindcss = {
           root_dir = function()
             return vim.fs.dirname(vim.fs.find(".git", { path = startpath, upward = true })[1])
           end,
