9b8e429a3c36f96198c91cf3df3712fe8fc60572
Denys Halenok <denys.halenok@gmail.com>
2025-10-27 21:21:53 +0100

    Minor fixes

--- .config/nvim.lazy/lua/plugins/fzf-lua.lua
+++ .config/nvim.lazy/lua/plugins/fzf-lua.lua
@@ -4,7 +4,7 @@ return {
   opts = { "telescope" },
   keys = {
     { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (Root Dir)" },
-    { "<leader>fg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (Root Dir)" },
+    { "<leader>fg", LazyVim.pick("live_grep", { root = false, hidden = true }), desc = "Grep (Root Dir)" },
     { "<c-P>", "<cmd>FzfLua git_files<cr>", desc = "Find Files (git-files)" },
     { "<leader>b", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
     { "<leader>gs", false },
